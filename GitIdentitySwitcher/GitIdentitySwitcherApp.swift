d4ac255f86ff12cf5589f72e7175b992c0743ced
alishapckg <alishaproton@proton.me>
2026-08-03 23:42:09 +0300

    Initial Commit

---
+++ GitIdentitySwitcher/GitIdentitySwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitIdentitySwitcherApp.swift
+//  GitIdentitySwitcher
+//
+//  Created by NS on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct GitIdentitySwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
