811cb51fc5a1ad972dbed82e1f886619413e96b2
alishapckg <alishaproton@proton.me>
2026-08-03 15:00:00 +0300

    removed content view code

--- GitIdentitySwitcher/ContentView.swift
+++ GitIdentitySwitcher/ContentView.swift
@@ -1,13 +1,1 @@
-import SwiftUI
 
-struct ContentView: View {
-  var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-
-    }
-    .padding()
-  }
-}
