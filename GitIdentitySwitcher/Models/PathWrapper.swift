474e629fba2fcebc09c7ed648c01bfe616091583
alishapckg <alishaproton@proton.me>
2026-08-08 15:00:00 +0300

    added repo list view

---
+++ GitIdentitySwitcher/Models/PathWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  PathWrapper.swift
+//  GitIdentitySwitcher
+//
+//  Created by NS on 8/9/26.
+//
+
+import Foundation
---
+++ GitIdentitySwitcher/RepoListView.swift
@@ -0,0 +1,8 @@
+//
+//  RepoListView.swift
+//  GitIdentitySwitcher
+//
+//  Created by NS on 8/9/26.
+//
+
+import Foundation
